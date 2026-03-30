c01c917f331c2dd117249e3ddcf543b30ee226eb
Skyrat <bahdenizler@gmail.com>
2026-03-30 16:57:08 -0400

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+X-Content-Type-Options: nosniff
 
 createRoot(document.getElementById("root")!).render(<App />);
