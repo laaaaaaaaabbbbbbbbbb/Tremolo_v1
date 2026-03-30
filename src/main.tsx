6141c8ed8cf7e57b386575620a8b5d1fc9dd060d
Petrus Yabancıses <99743682+Petrus-Main@users.noreply.github.com>
2026-03-30 19:09:26 -0400

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,4 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
-const myHeaders = new Headers();
-
-myHeaders.append("Content-Type", nosniff);
-myHeaders.get("Content-Type"); // should return 'text/xml'
-
 createRoot(document.getElementById("root")!).render(<App />);
